596efbbb2033b751887308944c6bd4e012471b6e
Kevin Baer <149030800+kevbaer@users.noreply.github.com>
2025-10-17 23:34:53 -0700

    added makeviz with only input dat

---
+++ 80_makeviz.R
@@ -0,0 +1,218 @@
+library(gganimate)
+library(glue)
+
+makeViz <- function(
+  inputDataset,
+  outputDataset,
+  playDataset,
+  gid,
+  pid,
+  club1,
+  club2,
+  club1col,
+  club2col,
+  yearNum,
+  weekNum,
+  yardlow = 0,
+  yardhigh = 120,
+  frameStart = NA,
+  frameEnd = NA,
+  toSave = FALSE,
+  saveName = NA,
+  endFreeze = 4,
+  startFreeze = 0,
+  annotate = FALSE
+) {
+  full_df <- inputDataset |>
+    filter(game_id == gid & play_id == pid) |>
+    left_join(playDataset, by = join_by(game_id, play_id)) |>
+    mutate(
+      player_club = ifelse(
+        player_side == "Defense",
+        defensive_team,
+        possession_team
+      )
+    )
+
+  if (is.na(frameStart)) {
+    begin <- full_df |> pull(frame_id) |> min()
+  } else {
+    begin <- frameStart
+  }
+  if (is.na(frameEnd)) {
+    end <- full_df |> pull(frame_id) |> max()
+  } else {
+    end <- frameEnd
+  }
+
+  length <- (end - begin) + 1 + endFreeze + startFreeze
+
+  ex <- full_df |>
+    mutate(
+      pt_color = case_when(
+        if (annotate) {
+          is_slot ~ "gold"
+        },
+        player_club == club1 ~ club1col,
+        player_club == club2 ~ club2col,
+      ),
+      pt_size = 3.8
+    )
+  desc <- playDataset |>
+    filter(game_id == gid & play_id == pid) |>
+    pull(play_description) |>
+    str_replace("\\)\\.", "\\)")
+
+  qNum <- playDataset |>
+    pull(quarter)
+
+  anim <- ggplot() +
+    annotate(
+      "text",
+      y = seq(20, 100, 10),
+      x = 10,
+      color = "#000000",
+      family = "sans",
+      label = c("1 0", "2 0", "3 0", "4 0", "5 0", "4 0", "3 0", "2 0", "1 0"),
+      angle = 270
+    ) +
+    annotate(
+      "text",
+      y = seq(20, 100, 10),
+      x = 40,
+      color = "#000000",
+      family = "sans",
+      label = c("1 0", "2 0", "3 0", "4 0", "5 0", "4 0", "3 0", "2 0", "1 0"),
+      angle = 90
+    ) +
+    annotate(
+      "text",
+      y = setdiff(seq(10, 110, 1), seq(10, 110, 5)),
+      x = 0,
+      color = "#000000",
+      label = "—",
+      angle = 0
+    ) +
+    annotate(
+      "text",
+      y = setdiff(seq(10, 110, 1), seq(10, 110, 5)),
+      x = 160 / 3,
+      color = "#000000",
+      label = "—",
+      angle = 0
+    ) +
+    annotate(
+      "text",
+      y = setdiff(seq(10, 110, 1), seq(10, 110, 5)),
+      x = 23.36667,
+      color = "#000000",
+      label = "–",
+      angle = 0
+    ) +
+    annotate(
+      "text",
+      y = setdiff(seq(10, 110, 1), seq(10, 110, 5)),
+      x = 29.96667,
+      color = "#000000",
+      label = "–",
+      angle = 0
+    ) +
+    annotate(
+      "segment",
+      x = c(-Inf, Inf),
+      xend = c(-Inf, Inf),
+      y = 0,
+      yend = 120,
+      color = "#000000"
+    ) +
+    geom_hline(yintercept = seq(10, 110, 5), color = "#000000") +
+    geom_point(
+      data = filter(
+        ex,
+        frame_id %in%
+          {
+            begin
+          }:{
+            end
+          }
+      ),
+      shape = 21,
+      aes(160 / 3 - x, y, size = pt_size, fill = pt_color)
+    ) +
+    scale_size_identity() +
+    scale_fill_identity() +
+    transition_time(frame_id) +
+    ease_aes("linear") +
+    coord_cartesian(
+      ylim = c(yardlow, yardhigh),
+      xlim = c(0, 160 / 3),
+      expand = FALSE
+    ) +
+    theme_minimal() +
+    labs(
+      title = glue(
+        "<span style = 'color:{club1col};'>**{club1}**</span> vs. <span style = 'color:{club2col};'>**{club2}**</span>, {yearNum} Week {weekNum}"
+      ),
+      subtitle = str_c("Q{qNum}: ", desc)
+    ) +
+    theme(
+      panel.background = element_rect(fill = "white"),
+      legend.position = "none",
+      plot.subtitle = element_text(size = 9, face = "italic", hjust = 0.5),
+      plot.title = ggtext::element_markdown(hjust = 0.5, size = 12),
+      text = element_text(family = "sans", color = "#000000"),
+      axis.text = element_blank(),
+      panel.grid = element_blank(),
+      axis.title = element_blank(),
+      axis.ticks = element_blank()
+    ) +
+    {
+      if (annotate) {
+        geom_label(
+          data = filter(
+            ex,
+            frameId %in%
+              {
+                begin
+              }:{
+                end
+              }
+          ),
+          aes(160 / 3 - x + 1, 64, label = deets, fill = "lightblue", size = 3)
+        )
+      }
+    }
+
+  tot <- yardhigh - yardlow
+
+  a1 <- animate(
+    nframes = length,
+    anim,
+    width = 700,
+    height = (40 * tot) / 3,
+    fps = 10,
+    end_pause = endFreeze,
+    start_pause = startFreeze,
+    res = 105,
+  )
+
+  if (toSave) {
+    anim_save(glue("{saveName}.gif"), a1)
+  }
+
+  return(a1)
+}
+
+makeViz(
+  input,
+  output,
+  supp,
+  2023090700,
+  101,
+  "KC",
+  "DET",
+  "red",
+  "blue",
+  "2023",
+  "05"
+)
--- 99_data_org.R
+++ 99_data_org.R
@@ -1,7 +1,53 @@
 library(tidyverse)
 library(arrow)
 
-input <- read_parquet("data/input.parquet")
-output <- read_parquet("data/output.parquet")
 
-a <- read_csv("data/supplementary_data.csv")
+output <- read_parquet("data/output.parquet")
+supp <- read_parquet("data/supp.parquet")
+
+
+input <- read_parquet("data/input.parquet") |>
+  mutate(player_to_predict = as.logical(player_to_predict)) |>
+  mutate(
+    new_y = if_else(play_direction == "left", 120 - x, x),
+    new_x = if_else(play_direction == "left", 160 / 3 - y, y),
+    new_ball_land_x = ifelse(
+      play_direction == "left",
+      160 / 3 - ball_land_y,
+      ball_land_y
+    ),
+    new_ball_land_y = ifelse(
+      play_direction == "left",
+      120 - ball_land_x,
+      ball_land_x
+    ),
+    dir = ifelse(play_direction == "left", dir + 180, dir),
+    dir = ifelse(dir > 360, dir - 360, dir),
+    o = ifelse(play_direction == "left", o + 180, o),
+    o = ifelse(o > 360, o - 360, o)
+  ) |>
+  mutate(
+    y = new_y,
+    x = new_x,
+    ball_land_x = new_ball_land_x,
+    ball_land_y = new_ball_land_y
+  ) |>
+  select(-c(new_x, new_y, new_ball_land_x, new_ball_land_y))
+
+output <- read_parquet("data/output.parquet") |>
+  left_join(
+    read_parquet("data/input.parquet") |>
+      select(game_id, play_id, play_direction) |>
+      unique(),
+    by = join_by(game_id, play_id)
+  ) |> 
+  mutate(
+    new_y = if_else(play_direction == "left", 120 - x, x),
+    new_x = if_else(play_direction == "left", 160 / 3 - y, y)
+) |> 
+  mutate(
+    y = new_y,
+    x = new_x,
+  ) |> 
+  select(-c(new_y, new_x))
+  
