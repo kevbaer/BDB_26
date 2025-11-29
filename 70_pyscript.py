5a090cfdc590f0356264a0cd263e53a91fe208ea
Kevin Baer <149030800+kevbaer@users.noreply.github.com>
2025-11-28 23:08:46 -0800

    started getting swarm score code up and going

--- 70_pyscript.py
+++ 70_pyscript.py
@@ -2,11 +2,106 @@
 import numpy as np
 import polars as pl
 from scipy import stats
+import pandas as pd
 
-dat_i = pl.read_parquet("data/input.parquet")
-dat_o = pl.read_parquet("data/output.parquet")
-supp = pl.read_parquet("data/supp.parquet")
 
-x, y = np.mgrid[0:(120/3):1, 0:120:1]
+at_throw_df = pl.read_parquet("transfer/at_throw_df.parquet")
+
+
+x, y = np.mgrid[0 : (120 / 3) : 1, 0:120:1]
 locations = np.dstack((x, y))
+mean_x = 0
+mean_y = 0
 
+
+def defense_pdf_builder(data, game, play):
+    defense_pdfs = []
+    for index, row in data.iterrows():
+        if (
+            row["game_id"] == game
+            and row["play_id"] == play
+            and row["player_side"] == "Defense"
+        ):
+            # the following steps are coded from Bornn and Fernandez's 2018 paper
+            # with assist from Inayatali, Hocevar, and White's 2023 BDB entry.
+            speedUsage = (row["s"] ** 2) / (121)
+            upLeftScaling = (
+                row["total_dist_from_ball"] + row["total_dist_from_ball"] * speedUsage
+            ) / 2
+            bottomRightScaling = (
+                row["total_dist_from_ball"] - row["total_dist_from_ball"] * speedUsage
+            ) / 2
+            r_matrix = [(row["hcomp"], -row["vcomp"]), (row["vcomp"], row["hcomp"])]
+            r_matrix = pd.DataFrame(data=r_matrix)
+            s_matrix = [
+                (upLeftScaling + 0.0000001, 0),
+                (0, bottomRightScaling - 0.0000001),
+            ]
+            s_matrix = pd.DataFrame(data=s_matrix)
+            inverse_r_Matrix = np.linalg.inv(r_matrix)
+            Matrixmult = r_matrix.dot(s_matrix)
+            nextMatrix = Matrixmult.dot(s_matrix)
+            covariance_matrix = nextMatrix.dot(inverse_r_Matrix)
+            mean_x = row["x"] + row["h_speed"] * 0.5
+            mean_y = row["y"] + row["v_speed"] * 0.5
+            means = [mean_x, mean_y]
+            # player_pdf creates the influence map
+            player_pdf = stats.multivariate_normal(means, covariance_matrix).pdf(
+                locations
+            )
+            # percent_player_pdf shows the percentage of their total influence at any one point
+            percent_player_pdf = player_pdf / np.sum(np.sum(player_pdf, axis=1), axis=0)
+            defense_pdfs.append(percent_player_pdf)
+
+    return defense_pdfs
+
+
+def receiver_pdf_builder(data, game, play):
+    m = 0
+    for index, row in data.iterrows():
+        if (
+            row["game_id"] == game
+            and row["play_id"] == play
+            and row["player_role"] == "Targeted Receiver"
+        ):
+            ballCarrierX = row["x"] + row["h_speed"] * 0.5
+            ballCarrierY = row["y"] + row["v_speed"] * 0.5
+            ballCarrierCoord = [ballCarrierX, ballCarrierY]
+            ballCarrier_pdf = stats.multivariate_normal(
+                [ballCarrierX, ballCarrierY], [[5, 0], [0, 5]]
+            ).pdf(locations)
+            m = 1
+            if m == 1:
+                return ballCarrier_pdf
+
+
+def evaluator(defenders, carrier):
+    values = []
+    for DefPlayerPDF in defenders:
+        result = np.array(DefPlayerPDF) * np.array(carrier)
+        swarm_val = np.sum(np.sum(result, axis=1), axis=0) / np.sum(
+            np.sum(carrier, axis=1), axis=0
+        )
+        values.append(swarm_val)
+    count = 0
+    for value in values:
+        if value > 0.0055:
+            # 0.0055 was the arbitrary value I found to give the results that most accurately represented the film
+            # (0.006 was more often than not impacting the play, while 0.005 was not.)
+            count = count + 1
+    return count
+
+
+# ----------- Do the work -----------
+rally_Score = np.array([])
+
+game_and_play_pairs = at_throw_df.select("game_id", "play_id").unique()
+
+for GS, PS in game_and_play_pairs.iter_rows():
+    swarm_Score = np.append(
+        rally_Score,
+        evaluator(
+            defense_pdf_builder(at_throw_df.to_pandas(), GS, PS),
+            receiver_pdf_builder(at_throw_df.to_pandas(), GS, PS),
+        ),
+    )
--- 99_data_org.R
+++ 99_data_org.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(arrow)
+library(DescTools)
 
 
-output <- read_parquet("data/output.parquet")
 supp <- read_parquet("data/supp.parquet")
 
 
@@ -32,7 +32,14 @@ input <- read_parquet("data/input.parquet") |>
     ball_land_x = new_ball_land_x,
     ball_land_y = new_ball_land_y
   ) |>
-  select(-c(new_x, new_y, new_ball_land_x, new_ball_land_y))
+  select(-c(new_x, new_y, new_ball_land_x, new_ball_land_y)) |>
+  mutate(h_dist_from_ball = abs(ball_land_x - x)) |>
+  mutate(v_dist_from_ball = abs(ball_land_y - y)) |>
+  mutate(total_dist_from_ball = (h_dist_from_ball^2 + v_dist_from_ball^2)^.5) |>
+  mutate(dir = DegToRad(dir)) |>
+  mutate(o = DegToRad(o)) |>
+  mutate(hcomp = cos(dir), vcomp = sin(dir)) |>
+  mutate(h_speed = hcomp * s, v_speed = vcomp * s)
 
 output <- read_parquet("data/output.parquet") |>
   left_join(
@@ -40,14 +47,53 @@ output <- read_parquet("data/output.parquet") |>
       select(game_id, play_id, play_direction) |>
       unique(),
     by = join_by(game_id, play_id)
-  ) |> 
+  ) |>
   mutate(
     new_y = if_else(play_direction == "left", 120 - x, x),
     new_x = if_else(play_direction == "left", 160 / 3 - y, y)
-) |> 
+  ) |>
   mutate(
     y = new_y,
     x = new_x,
-  ) |> 
-  select(-c(new_y, new_x))
-  
+  ) |>
+  select(-c(new_y, new_x)) |>
+  mutate(
+    x_lag_1 = lag(x, n = 1),
+    x_lag_2 = lag(x, n = 2),
+    x_lag_3 = lag(x, n = 3),
+    y_lag_1 = lag(y, n = 1),
+    y_lag_2 = lag(y, n = 2),
+    y_lag_3 = lag(y, n = 3),
+    .by = c(game_id, play_id, nfl_id)
+  ) |>
+  mutate(
+    s_estimate = sqrt((x_lag_3 - x_lag_1)**2 + (y_lag_3 - y_lag_1)**2) / .2
+  )
+
+
+play_info <- input |>
+  filter(player_to_predict) |>
+  select(
+    game_id,
+    play_id,
+    nfl_id,
+    player_name,
+    player_side,
+    player_role,
+    num_frames_output,
+    ball_land_x,
+    ball_land_y
+  ) |>
+  distinct()
+
+
+enhanced_output <- output |>
+  left_join(play_info, by = join_by(game_id, play_id, nfl_id)) |>
+  select(-play_direction)
+
+
+at_throw_df <- input |>
+  filter(frame_id == (max(frame_id) - 5), .by = c(game_id, play_id, nfl_id)) |>
+  filter(player_to_predict)
+
+# write_parquet(at_throw_df, "transfer/at_throw_df.parquet")
